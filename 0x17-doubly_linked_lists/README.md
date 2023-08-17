548c477316224cb52ddf000be80b67fac7140814
hancho6319 <abbaauwalharuna@gmail.com>
2023-08-17 17:52:38 +0000

    my commit

---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+#This is the REAME file of 0x17. C - Doubly linked lists
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* This is the header files*/
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* The doubly linked list struct node */
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* The prototypes of the functions */
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
+#endif
